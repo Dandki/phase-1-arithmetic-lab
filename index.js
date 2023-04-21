e9e8970ec5405d1fd572bee45ca00506d7041772
John Dasalla <johndasallaz@hotmail.com>
2023-04-21 09:52:48 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Multiply
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+// Random 
+let random = Math.round(Math.random()*20);
+
+// Mod
+const num3 = 32;
+const num4 = 8;
+let mod = num3 / num4;
+
+// Max
+let max = Math.max(5,10,15,20);
